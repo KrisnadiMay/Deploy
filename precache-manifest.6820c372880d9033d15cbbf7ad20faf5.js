eb0b5720bcb7728bac19523e19d2dae04ca8d16a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-18 02:55:38 +0000

    Deploy KrisnadiMay/Linok to github.com/KrisnadiMay/Linok.git:gh-pages

---
+++ precache-manifest.6820c372880d9033d15cbbf7ad20faf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17c9fd99fa2236154ec3",
+    "url": "/Linok/static/css/main.2011f063.chunk.css"
+  },
+  {
+    "revision": "17c9fd99fa2236154ec3",
+    "url": "/Linok/static/js/main.17c9fd99.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/Linok/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "78d7a95310c991cdc12d",
+    "url": "/Linok/static/js/runtime~main.78d7a953.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Linok/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cef579b2579317bc42219e4524c37f0d",
+    "url": "/Linok/index.html"
+  }
+];
